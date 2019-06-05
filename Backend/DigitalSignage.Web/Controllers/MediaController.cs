06578a36f939df85feb176ec5dae9c6fc6a2ae25
Leon Rahman <leon.rahman@teorema.net>
2019-06-05 14:27:24 +0200

    Aggiunta api funzio

---
+++ Backend/DigitalSignage.Web/Controllers/MediaController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Microsoft.AspNetCore.Mvc;
+using DAL;
+
+namespace DigitalSignage.Web.Controllers
+{
+    [Route("api/media")]
+    [ApiController]
+    public class MediaController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        [Route("GetAll")]
+        public List<Media> GetAll()
+        {
+            Gestore g = new Gestore();
+            var todos = g.GetTodos();
+            return todos;
+        }
+
+        //// GET api/values/5
+        //[HttpGet("{id}")]
+        //public ActionResult<string> Get(int id)
+        //{
+        //    return "value";
+        //}
+
+        //// POST api/values
+        //[HttpPost]
+        //public void Post([FromBody] string value)
+        //{
+        //}
+
+        //// PUT api/values/5
+        //[HttpPut("{id}")]
+        //public void Put(int id, [FromBody] string value)
+        //{
+        //}
+
+        //// DELETE api/values/5
+        //[HttpDelete("{id}")]
+        //public void Delete(int id)
+        //{
+        //}
+    }
+}
