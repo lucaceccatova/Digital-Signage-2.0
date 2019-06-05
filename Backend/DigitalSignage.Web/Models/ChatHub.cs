807d633be36dc7ff3bf4f0dfb6a32a6f07d0fdd3
Luca Ceccato <luca.ceccato@teorema.net>
2019-06-05 09:55:52 +0200

    Aggiunta solution backend con esempio di SignalR

---
+++ Backend/DigitalSignage.Web/Models/ChatHub.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace DigitalSignage.Web.Models
+{
+    public class ChatHub : Hub
+
+    {
+
+        public void SendToAll(string name, string message)
+
+        {
+
+            Clients.All.SendAsync("sendToAll", name, message);
+
+        }
+
+    }
+}
