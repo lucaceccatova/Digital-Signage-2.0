329a4d3fc46abbc5b1a7360cec639e2d6d15d844
lucaceccatova <lucaceccatova@gmail.com>
2019-06-14 11:46:21 +0200

    Now signal can return value to the web application

--- WebApplication1/WebApplication1/Models/VoiceHub.cs
+++ WebApplication1/WebApplication1/Models/VoiceHub.cs
@@ -1,38 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using Microsoft.AspNetCore.SignalR;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.SignalR;
-using BLL;
-
 
 namespace WebApplication1.Models
 {
-    public class VoiceHub :Hub
+    public class VoiceHub : Hub
     {
-        //public void SendToAll(string name, string message)
-
-        //{
-
-        //    Clients.All.SendAsync("sendToAll", name, message);
-
-        //}
-
-        public async Task SendToAll(string id)
+        public  async Task SendMessage(int id)
         {
-            //List<MediaBL> mx = new List<MediaBL>();
-            //mx = GestoreBLL.GetMedia();
-
-            //try
-            //{
-            //    MediaBL m = (MediaBL)mx.Where(p => p.id.Equals(int.Parse(id)));
-            //    Clients.All.SendAsync("sendID", m.id);
-            //}
-            //catch (Exception)
-            //{
-            //    Clients.All.SendAsync("sendID", "ID NON VALIDO");
-            //}
-           await Clients.All.SendAsync("sendID", 2);
+             await Clients.All.SendAsync("ReceiveMessage", id);
         }
+
+
     }
 }
