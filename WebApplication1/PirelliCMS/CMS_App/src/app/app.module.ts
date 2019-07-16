845a85cbbfaab6698767d96282a0f04a5fa219f1
lucaceccatova <lucaceccatova@gmail.com>
2019-07-16 16:20:58 +0200

    FE fix

--- WebApplication1/PirelliCMS/CMS_App/src/app/app.module.ts
+++ WebApplication1/PirelliCMS/CMS_App/src/app/app.module.ts
@@ -5,19 +5,19 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatToolbarModule, MatIcon} from '@angular/material'
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { DashboardElementComponent } from './component/dashboard-element/dashboard-element.component';
-import { UploadComponent } from './component/upload/upload.component';
 import { FeatureRoutingModule } from './router-module';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule,MatCardModule} from '@angular/material';
 import {MatSliderModule} from '@angular/material/slider';
-import {MatSelectModule, matSelectAnimations} from '@angular/material/select';
+import {MatSelectModule} from '@angular/material/select';
 import { HttpClientModule } from '@angular/common/http';
 import {MatProgressBarModule} from '@angular/material/progress-bar';
 import {MatButtonModule} from '@angular/material/button';
 import { MaterialFileInputModule } from 'ngx-material-file-input';
 import {MatIconModule} from '@angular/material/icon';
 import { HomeComponent } from './component/home/home.component';
-import { CategoryComponent } from './component/category/category.component';
+import { CategoryComponent } from './component/category/categoryAdd/category.component.add';
+import { UploadComponent } from './component/upload/mediaAdd/upload.component';
 @NgModule({
   declarations: [
     AppComponent,
