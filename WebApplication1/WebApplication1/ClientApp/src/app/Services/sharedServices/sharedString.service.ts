80668741ff76367d9df637506688a615187a2d12
lucaceccatova <lucaceccatova@gmail.com>
2019-07-24 10:45:45 +0200

    bug fix

--- WebApplication1/WebApplication1/ClientApp/src/app/Services/sharedServices/sharedString.service.ts
+++ WebApplication1/WebApplication1/ClientApp/src/app/Services/sharedServices/sharedString.service.ts
@@ -2,7 +2,11 @@ import { Injectable } from '@angular/core';
 import { element } from 'src/app/Models/Element';
 import { videoPage } from 'src/app/Models/videoPage';
 
-@Injectable()
+@Injectable(
+    {
+        providedIn:'root',
+    }
+)
 export class sharedStringService {
     singleVideo:element;
     pages:videoPage[]=null;
