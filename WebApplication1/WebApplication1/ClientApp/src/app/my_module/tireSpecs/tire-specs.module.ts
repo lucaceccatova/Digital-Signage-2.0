e021780b06af394280b4fbc19b00cf8b4b4dfa41
lucaceccatova <lucaceccatova@gmail.com>
2019-07-29 13:50:56 +0200

    Working version with fixed animations

--- WebApplication1/WebApplication1/ClientApp/src/app/my_module/tireSpecs/tire-specs.module.ts
+++ WebApplication1/WebApplication1/ClientApp/src/app/my_module/tireSpecs/tire-specs.module.ts
@@ -4,11 +4,13 @@ import { tireSpecsComponent } from './tire-specs.component';
 import { tireSpecsRouter } from './tire-specs.router';
 import { sharedStringService } from 'src/app/Services/sharedServices/sharedString.service';
 import { preserveSpecService } from 'src/app/Services/preserveSpecComponent/preserveSpec.service';
+import { MatSnackBarModule } from '@angular/material';
 
 @NgModule({
-    declarations: [tireSpecsComponent],
-    imports: [ CommonModule,tireSpecsRouter ],
-    exports: [],
+    declarations: [tireSpecsComponent
+    ],
+    imports: [ CommonModule,tireSpecsRouter,MatSnackBarModule, ],
     providers: [sharedStringService,preserveSpecService],
+    entryComponents:[]
 })
 export class tireSpecsModule {}
